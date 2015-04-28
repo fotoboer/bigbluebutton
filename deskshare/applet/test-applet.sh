8661a864560e8cf75c31f336805b5b5e52655337
Fotoboer <github@fotoboer.nl>
2015-04-28 13:22:07 +0200

    changed applet version to 090

--- deskshare/applet/test-applet.sh
+++ deskshare/applet/test-applet.sh
@@ -3,5 +3,5 @@ gradle clean
 cd ../applet
 gradle jar
 ant sign-certificate-jar
-cp /home/firstuser/dev/bigbluebutton/deskshare/applet/build/libs/bbb-deskshare-applet-0.8.1.jar /home/firstuser/dev/bigbluebutton/bigbluebutton-client/client/
+cp /home/firstuser/dev/bigbluebutton/deskshare/applet/build/libs/bbb-deskshare-applet-0.9.0.jar /home/firstuser/dev/bigbluebutton/bigbluebutton-client/client/
 
